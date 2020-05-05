6d6b53aa93db74f773dd8591d2b93a930b9a1fdb
Nevin Manimala <53708323+nm11g-rgb@users.noreply.github.com>
2020-05-05 18:26:03 -0400

    Update wg-ububtu-server-up.sh

--- wg-ububtu-server-up.sh
+++ wg-ububtu-server-up.sh
@@ -50,8 +50,8 @@ echo ---------------------------------------------------configure firewall rules
 sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p udp -m udp --dport 55000 -m conntrack --ctstate NEW -j ACCEPT
-sudo iptables -A INPUT -s 10.0.0.0/24 -p tcp -m tcp --dport 5335 -m conntrack --ctstate NEW -j ACCEPT
-sudo iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 5335 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A INPUT -s 10.0.0.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 
 # make firewall changes persistent
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
@@ -65,8 +65,8 @@ sudo netfilter-persistent save
 echo ---------------------------------------------install and configure unbound
 sudo apt install -y unbound unbound-host
 
-curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.root
-echo 'curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.root' > /etc/cron.monthly/curl_root_hints.sh
+curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache
+echo 'curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache' > /etc/cron.monthly/curl_root_hints.sh
 chmod +x /etc/cron.monthly/curl_root_hints.sh
 
 
@@ -88,19 +88,9 @@ cat > /etc/unbound/unbound.conf.d/pi-hole.conf << ENDOFFILE
 server:
     # If no logfile is specified, syslog is used
     # logfile: "/var/log/unbound/unbound.log"
-    num-threads: 4
-    # disable logs
     verbosity: 0
 
-    # respond to DNS requests on all interfaces
-    interface: 0.0.0.0
-    max-udp-size: 3072
-    # IPs authorised to access the DNS Server
-    access-control: 0.0.0.0/0                 refuse
-    access-control: 127.0.0.1                 allow
-    access-control: 10.0.0.0/24             allow
-    # not allowed to be returned for public Internet  names
-    private-address: 10.0.0.0/24
+    interface: 127.0.0.1
     port: 5335
     do-ip4: yes
     do-udp: yes
@@ -115,38 +105,28 @@ server:
 
     # Use this only when you downloaded the list of primary root servers!
     root-hints: "/var/lib/unbound/root.hints"
-    
-    # use the root server's key for DNSSEC
-    auto-trust-anchor-file: "/var/lib/unbound/root.key"
 
-    #hide DNS Server info
-    hide-identity: yes
-    hide-version: yes
     # Trust glue only if it is within the server's authority
     harden-glue: yes
-    harden-referral-path: yes
 
     # Require DNSSEC data for trust-anchored zones, if such data is absent, the zone becomes BOGUS
     harden-dnssec-stripped: yes
-    # add an unwanted reply threshold to clean the cache and avoid, when possible, DNS poisoning
-    unwanted-reply-threshold: 10000000
-    # have the validator print validation failures to the log
-    val-log-level: 1
-    # minimum lifetime of cache entries in seconds
-    cache-min-ttl: 1800
-    # maximum lifetime of cached entries in seconds
-    cache-max-ttl: 14400
+
+    # Don't use Capitalization randomization as it known to cause DNSSEC issues sometimes
+    # see https://discourse.pi-hole.net/t/unbound-stubby-or-dnscrypt-proxy/9378 for further details
+    use-caps-for-id: no
+
+    # Reduce EDNS reassembly buffer size.
+    # Suggested by the unbound man page to reduce fragmentation reassembly problems
+    edns-buffer-size: 1472
+
+    # Perform prefetching of close to expired message cache entries
+    # This only applies to domains that have been frequently queried
     prefetch: yes
-    prefetch-key: yes
 
     # One thread should be sufficient, can be increased on beefy machines. In reality for most users running on small networks or on a single machine, it should be unnecessary to seek performance enhancement by increasing num-threads above 1.
     num-threads: 1
-    # don't use Capitalization randomization as it known to cause DNSSEC issues sometimes
-    # see https://discourse.pi-hole.net/t/unbound-stubby-or-dnscrypt-proxy/9378 for further details
-    use-caps-for-id: no
-    # reduce EDNS reassembly buffer size.
-    # suggested by the unbound man page to reduce fragmentation reassembly problems
-    edns-buffer-size: 1472
+
     # Ensure kernel buffer is large enough to not lose messages in traffic spikes
     so-rcvbuf: 1m
 
@@ -154,7 +134,7 @@ server:
     private-address: 192.168.0.0/16
     private-address: 169.254.0.0/16
     private-address: 172.16.0.0/12
-    private-address: 10.0.0.0/24
+    private-address: 10.0.0.0/8
     private-address: fd00::/8
     private-address: fe80::/10
 ENDOFFILE
